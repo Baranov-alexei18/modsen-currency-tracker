33c255f5384d0d8d7394cf6aa3b19c28e52eab68
Alexei Baranov <alexei-baranov18@mail.ru>
2024-03-14 11:14:36 +0300

    style: added assets img, style, font

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,5 @@
+import '@/assets/style/index.scss';
+
 import React, { Suspense } from 'react';
 import { createRoot } from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
