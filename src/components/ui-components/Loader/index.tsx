11f21169cf7f8f13411d5bfceb801a9ebc2c22c5
Alexei Baranov <alexei-baranov18@mail.ru>
2024-03-20 12:46:13 +0300

    feat(loader): create Loader and use in components

---
+++ src/components/ui-components/Loader/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import classes from './styles.scss';
+
+export const Loader = () => (
+  <div className={classes.wrapper}>
+    <div className={classes.loader} />
+  </div>
+);
